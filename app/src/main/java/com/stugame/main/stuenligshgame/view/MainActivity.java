def70485c3fcf176a8f331e6baa7a4573a399d75
317095153@qq.com <317095153@qq.com>
2016-04-22 20:36:17 +0800

    init the project

---
+++ app/src/main/java/com/stugame/main/stuenligshgame/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.stugame.main.stuenligshgame.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stugame.main.stuenligshgame.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
