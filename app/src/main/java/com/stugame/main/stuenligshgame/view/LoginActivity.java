8acc0a72738322ecc6fd0290c016fef77c39e007
317095153@qq.com <317095153@qq.com>
2016-04-26 12:40:28 +0800

    添加了toolbar的显示

--- app/src/main/java/com/stugame/main/stuenligshgame/view/LoginActivity.java
+++ app/src/main/java/com/stugame/main/stuenligshgame/view/LoginActivity.java
@@ -1,15 +1,45 @@
 package com.stugame.main.stuenligshgame.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.stugame.main.stuenligshgame.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.usernameEditText)
+    EditText usernameEditText;
+
+    @Bind(R.id.passwordEditText)
+    EditText passwordEditText;
+
+    @Bind(R.id.loginButton)
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+//      设置toolbar
+        toolbar.setTitle("Login");
+        setSupportActionBar(toolbar);
+
+    }
+    @OnClick(R.id.loginButton)
+    public void onClick() {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
     }
 }
--- app/src/main/java/com/stugame/main/stuenligshgame/view/MainActivity.java
+++ app/src/main/java/com/stugame/main/stuenligshgame/view/MainActivity.java
@@ -1,15 +1,33 @@
 package com.stugame.main.stuenligshgame.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.stugame.main.stuenligshgame.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private final String TAG = this.getClass().getName();
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+//      设置toolbar
+        toolbar.setTitle(R.string.app_name);
+
+        setSupportActionBar(toolbar);
     }
 }
