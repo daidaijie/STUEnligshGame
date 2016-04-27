f4f2144a5db3916a23d7cd20f4ee98f3cc98a8fe
WindIsFlying <13xfzhang@stu.edu.cn>
2016-04-27 09:36:41 +0800

    增加了CreateQuizOrSurveyActivity 和 SeeQuestionActivity以及这两个对应的布局

---
+++ app/src/main/java/com/stugame/main/stuenligshgame/view/CreateQuizOrSurveyActivity.java
@@ -0,0 +1,7 @@
+package com.stugame.main.stuenligshgame.view;
+
+/**
+ * Created by xfzhang on 2016/4/27.
+ */
+public class CreateQuizOrSurveyActivity {
+}
---
+++ app/src/main/java/com/stugame/main/stuenligshgame/view/SeeQuestionActivity.java
@@ -0,0 +1,18 @@
+package com.stugame.main.stuenligshgame.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.stugame.main.stuenligshgame.R;
+
+/**
+ * Created by xfzhang on 2016/4/27.
+ */
+public class SeeQuestionActivity extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.see_question_layout);
+    }
+}
